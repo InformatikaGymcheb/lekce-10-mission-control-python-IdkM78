d47f2a10e5bad939dabd4925eac24df5816afca7
IdkM78 <dapilar@gymcheb.cz>
2026-04-28 08:34:47 +0200

    Update main.py

--- Lekce_10_Mission Control Python (USA & Space)/main.py
+++ Lekce_10_Mission Control Python (USA & Space)/main.py
@@ -13,9 +13,21 @@ def vypocet_vahy(vaha_na_zemi, teleso="mesic"):
     """
     # BONUS: Sem doplň kontrolu, zda vaha_na_zemi není záporná.
     # Pokud ano, vypiš varování nebo vrať 0.
+
+    vysledek = 0 
+    
+    if (vaha_na_zemi < 0):
+        print("Váha nesmí být záporná")
+        return 0
+    else:
+        if (teleso == "mesic"): 
+            vysledek = vaha_na_zemi * 0.165
+        elif (teleso == "mars"):
+            vysledek = vaha_na_zemi * 0.38
+        else:
+            print("špatné těleso")
     
     # TODO: Pomocí podmínek if/elif/else spočítej výslednou váhu
-    vysledek = 0 
     
     return vysledek
 
@@ -32,14 +44,25 @@ def simulator_pristani():
     print("\n--- ZAČÍNÁ PŘISTÁVACÍ MANÉVR ---")
     
     # TODO: Uprav podmínku cyklu, aby běžel, dokud je modul nad zemí (vyska > 0)
-    while False: 
+    while vyska > 0: 
         print(f"Výška: {vyska}m | Rychlost: {rychlost}m/s | Palivo: {palivo}j")
         
         # TODO: Získej od uživatele sílu zážehu motorů (vstup od 0 do 10)
         # Nezapomeň převést vstup na celé číslo (int).
-        zazeh = 0      # místo nuly bude vstup od uživatele a dojde ke zpomalení pádu
+        zazeh = int(input("zadej sílu zážehu motorů (od 0 do 10)"))      # místo nuly bude vstup od uživatele a dojde ke zpomalení pádu
         
         # TODO: Odečti použitý zážeh od paliva, musíme zařídit, aby „nádrž“ modulu postupně ubývala (pozor, nesmí jít do mínusu!)
+
+        if zazeh < 0:
+            zazeh = 0
+        elif zazeh > 10:
+            zazeh = 10
+        
+        palivo -= zazeh
+
+        rychlost += gravitace - zazeh
+
+        vyska -= rychlost
         
         # --- FYZIKÁLNÍ VÝPOČET ---
         # TODO: Aktualizuj rychlost (přičti gravitaci, odečti zážeh)
@@ -53,11 +76,17 @@ def simulator_pristani():
     
     # TODO: Doplň podmínku - pokud je rychlost menší než 5, mise je úspěšná.
     # Jinak modul havaroval.
-    print("Výsledek mise: ...")
+
+    if (rychlost < 5):
+        print("Výsledek mise: úspěch :)")
+    else:
+        print("Výsledek mise: neúspěch :(")
+    
 
 
 # === HLAVNÍ PROGRAM ===
 # Zde si student vyzkoušej své funkce
+
 moje_vaha = 80
 print(f"Moje váha na Měsíci: {vypocet_vahy(moje_vaha, 'mesic')} kg")
 
